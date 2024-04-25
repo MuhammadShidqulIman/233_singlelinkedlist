077103f7af5fe049ddd93ee9679a7308a071141a
MuhammadShidqulIman <muhammadshidqul3@gmail.com>
2024-04-25 15:20:07 +0700

    searchData

--- Single Linked List C++/Single Linked List C++.cpp
+++ Single Linked List C++/Single Linked List C++.cpp
@@ -105,6 +105,15 @@ void traverse() {
     }
 }
 
+void searchData() {
+    if (listEmpty()) {
+        cout << "List Kosong" << endl;
+        system("pause");
+        system("cls");
+        return;
+    }
+}
+
 
 int main()
 {
